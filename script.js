5a44c55576b5138506bd13f4164f7133cb6427f5
tfu2003 <officialtonyfu@gmail.com>
2022-11-14 21:45:36 -0800

    Allowed saving elements to to do list possible

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
 const form = document.getElementById("todoform");
+const input = document.getElementById("newtodo");
+let todos = [];
+
 form.addEventListener("submit", function (event) {
   event.preventDefault();
+  saveToDo();
 });
+
+const saveToDo = () => {
+  const newToDo = input.value;
+  const todo = {
+    value : newToDo,
+    checked : false,
+    color : '#FFFFFF'
+  }
+}
