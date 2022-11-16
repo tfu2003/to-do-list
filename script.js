89acc159042004213bc9723f18b97ff106767aeb
tfu2003 <officialtonyfu@gmail.com>
2022-11-15 23:16:41 -0800

    Checks to see if to do input is already in list

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 const form = document.getElementById("todoform");
 const input = document.getElementById("newtodo");
+const todoList = document.getElementById("todos-list");
 let todos = [];
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
   saveToDo();
+  createToDos();
 });
 
 const saveToDo = () => {
   const newToDo = input.value;
-  const todo = {
-    value : newToDo,
-    checked : false,
-    color : '#FFFFFF'
+  const lowerToDos = todos.map(function(value) {return value.toLowerCase()});
+  if (newToDo === '') {
+    alert("To do cannot be empty.");
+  } else if (lowerToDos.includes(newToDo.toLowerCase())) {
+    alert("To do is already in the list.");
+  } else {
+    const todo = {
+      value : newToDo,
+      checked : false,
+      color : '#FFFFFF'
+    }
   }
+  todos.push(newToDo);
+  input.value = '';
 }
+
+const createToDos = () => {}
